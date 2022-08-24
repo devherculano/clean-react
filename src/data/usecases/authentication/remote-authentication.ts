f6eec94834b44a6342ce08f4976c5ef051b822a8
Herculano <herculano@nextjstack.com>
2022-08-24 07:34:43 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,11 +3,12 @@ import { HttpPostClient } from "@/data/protocols/http/http-post-client";
 import { HttpStatusCode } from "@/data/protocols/http/http-response";
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
 import { UnexpectedError } from "@/domain/errors/unexpected-error";
+import { AccountModel } from "@/domain/models/account-model";
 
 export class RemoteAuthentication {
   constructor (
     private readonly url: string,
-    private readonly HttpPostClient: HttpPostClient
+    private readonly HttpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
     ) {}
 
   async auth (params: AuthenticationParams): Promise<void> {
@@ -19,7 +20,6 @@ export class RemoteAuthentication {
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: break
       case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
-      case HttpStatusCode.badRequest: throw new UnexpectedError()
       default: throw new UnexpectedError()
     }
 
