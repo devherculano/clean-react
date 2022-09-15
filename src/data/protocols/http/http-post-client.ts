e14b0f05e337a9a447f7f789300d652b652829f2
Herculano <herculano@nextjstack.com>
2022-09-15 09:49:11 -0300

    refactor: test refactor

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -3,6 +3,7 @@ import { HttpResponse } from "."
 export type HttpPostParams<T> = {
   url: string
   body?: T
+  
 }
 export interface HttpPostClient<T, R> {
   post (params: HttpPostParams<T>): Promise<HttpResponse<R>>
